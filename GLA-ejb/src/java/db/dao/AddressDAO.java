ebbd4741f786fac1141f074025f4703cc5f02429
Zhephyr54 <ale.lanoix@gmail.com>
2018-01-16 16:07:19 +0100

    [ADD]DAO classes + LocalDateTimeConverter for database timestamp type mapping

---
+++ GLA-ejb/src/java/db/dao/AddressDAO.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db.dao;
+
+import entity.Address;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author alexis
+ */
+@Stateless
+@LocalBean
+public class AddressDAO extends AbstractDAO<Address> {
+    
+    @PersistenceContext(unitName = "imp-pu")
+    private EntityManager em;
+
+    public AddressDAO() {
+        super(Address.class);
+    }
+    
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    } 
+
+}
---
+++ GLA-ejb/src/java/db/dao/SubcategoryDAO.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package db.dao;
+
+import entity.Subcategory;
+import javax.ejb.Stateless;
+import javax.ejb.LocalBean;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author alexis
+ */
+@Stateless
+@LocalBean
+public class SubcategoryDAO extends AbstractDAO<Subcategory> {
+    
+    @PersistenceContext(unitName = "imp-pu")
+    private EntityManager em;
+
+    public SubcategoryDAO() {
+        super(Subcategory.class);
+    }
+    
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    } 
+
+}
