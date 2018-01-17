151e31937d179f66e2d2dcfbdea930eb921d87e7
yasar demir <yasarfb66@hotmail.fr>
2018-01-17 21:44:06 +0100

    inscription/connexion/deconnexion

--- GLA-war/src/java/servlets/SignIn.java
+++ GLA-war/src/java/servlets/SignIn.java
@@ -1,45 +1,58 @@
 package servlets;
 
-import db.dao.UserDAO;
 import entity.User;
 import java.io.IOException;
-import javax.ejb.EJB;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import login.SignInForm;
-
-
+import static servlets.CreateAccount.URL_REDIRECTION;
 
 public class SignIn extends HttpServlet {
+
     public static final String ATT_USER = "utilisateur";
     public static final String ATT_FORM = "form";
-    public static final String VUE = "/Register.jsp";
-    
-    @EJB
-    UserDAO ud;
-		
-    public void doGet( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
-        /* Affichage de la page d'inscription */
-        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+    public static final String ATT_SESSION_USER = "sessionUtilisateur";
+    public static final String VUE = "/SignIn.jsp";
+    public static final String URL_REDIRECTION = "/GLA-war";
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        /* Affichage de la page de connexion */
+        this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
     }
-	
-    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         /* Préparation de l'objet formulaire */
         SignInForm form = new SignInForm();
-		
-        /* Appel au traitement et à la validation de la requête, et récupération du bean en résultant */
-        User utilisateur = form.inscrireUtilisateur( request );
-        if(form.isOk())
-        ud.create(utilisateur);
 
-		
+        /* Traitement de la requête et récupération du bean en résultant */
+        User utilisateur = form.connecterUtilisateur(request);
+
+        /* Récupération de la session depuis la requête */
+        HttpSession session = request.getSession();
+
+        /**
+         * Si aucune erreur de validation n'a eu lieu, alors ajout du bean
+         * Utilisateur à la session, sinon suppression du bean de la session.
+         */
+        if (form.getErreurs().isEmpty()) {
+            session.setAttribute(ATT_SESSION_USER, utilisateur);
+            /* Redirection vers la page de Connexion */
+            response.sendRedirect(URL_REDIRECTION);
+
+        } else {
+            session.setAttribute(ATT_SESSION_USER, null);
+        }
+
         /* Stockage du formulaire et du bean dans l'objet request */
-        request.setAttribute( ATT_FORM, form );
-        request.setAttribute( ATT_USER, utilisateur );
-		
-        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+        request.setAttribute(ATT_FORM, form);
+        request.setAttribute(ATT_USER, utilisateur);
+
+        if (!form.getErreurs().isEmpty()) {
+            this.getServletContext().getRequestDispatcher(VUE).forward(request, response);
+        }
     }
 }
